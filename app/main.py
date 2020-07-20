6de42b91371f8d5d2f231c5f0532af7bf4ea14ea
Luke Rogers <luke@dmptr.com>
2020-07-20 22:06:33 +1200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,66 @@
+import json
+from datetime import datetime
+
+import uvicorn
+from starlette.applications import Starlette
+from starlette.config import Config
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from starlette.responses import PlainTextResponse
+
+config = Config('.env')
+APPS_FILE = config('APPS_FILE')
+
+with open(APPS_FILE) as f:
+    app_list = json.load(f)
+
+templates = Jinja2Templates(directory='/code/templates')
+
+app = Starlette(debug=True)
+app.mount('/static', StaticFiles(directory='/code/static'), name='static')
+
+
+@app.route('/')
+async def homepage(request):
+    template = "index.html"
+
+    user = request.headers.get('remote-user', None)
+    groups = request.headers.get('remote-groupa', None)
+
+    now = datetime.now()
+
+    if now.hour < 5:
+        greeting = 'Good night'
+    elif now.hour < 12:
+        greeting = 'Good morning'
+    elif now.hour < 17:
+        greeting = 'Good afternoon'
+    elif now.hour < 22:
+        greeting = 'Good evening'
+    else:
+        greeting = 'Good night'
+
+    if user:
+        user = user.title()
+        greeting = f'{greeting}, {user}'
+
+    greeting += '!'
+
+    context = {
+        "request": request,
+        "greeting": greeting,
+        "apps": app_list,
+        "user": user,
+        "now": now
+
+    }
+    return templates.TemplateResponse(template, context)
+
+
+@app.route('/healthcheck')
+async def healthcheck(request):
+    return PlainTextResponse('Hello, world!')
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
